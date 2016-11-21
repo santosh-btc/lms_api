65598dc1f96957c28840b34e2b7e49ea4d3c4e02
santosh sharma <santosh.sharma@botreetechnologies.com>
2016-11-21 16:15:38 +0530

    - add lms api

---
+++ app/views/leave_types/_leave_type.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! leave_type, :id, :name, :descripton, :created_at, :updated_at
+json.url leave_type_url(leave_type, format: :json)
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  resources :holidays
+  resources :leave_types
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
